004ffdc1f8ad15733e305366136561a1702e89c1
Ram <payyalaramkumarchowdary@gmail.com>
2023-04-14 13:14:50 +0530

    resumelink-updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => (
       <SectionText>
       This is Ramkumar, a B.Tech CSE Undergrad and aspiring frontend web developer. I love exploring various designs and building websites out of them. I enjoy making websites responsive, accessible, and pixel-perfect.
       </SectionText>
-      <Button onClick={() => window.location ='https://github.com/Ramkumarchowdary' }>Know More</Button>
+      <Button onClick={() => window.location ='https://drive.google.com/file/d/1pd8ZFvehMlnqRqUmgvMhw8UiF5qpAury/view?usp=sharing' }>My Resume</Button>
     </LeftSection>
 
   </Section>
